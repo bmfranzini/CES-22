6c00d486204ee334e567a7a687c53dad98f85cc6
bmfranzini <101676549+bmfranzini@users.noreply.github.com>
2022-07-07 19:24:41 -0300

    Update bridge.py

--- Lab04_CES22/bridge.py
+++ Lab04_CES22/bridge.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
-
+# classe abstrata para a implementação do pattern bridge
 class Abstraction:
 
     def __init__(self, implementation: Implementation) -> None:
@@ -11,14 +11,14 @@ class Abstraction:
         return (f" com a tecnologia:\n"
                 f"\t{self.implementation.descricao_motor()}")
 
-
+# implementacao das caracteristicas que serao modeladas pela ponte (tipo do motor)
 class Implementation(ABC):
 
     @abstractmethod
     def descricao_motor(self) -> str:
         pass
 
-
+# classes concretas de tipos de motores herdadas da abstrata
 class MotorEletrico(Implementation):
     def descricao_motor(self) -> str:
         return "Motor elétrico."
@@ -33,7 +33,7 @@ class MotorHibrido(Implementation):
     def descricao_motor(self) -> str:
         return "Motor híbrido."
 
-
+# funcao de requisicao que recebe uma instancia da classe abstrata "Abstraction" 
 def client_code_bridge(abstraction: Abstraction) -> None:
     print(abstraction.descricao(), end="")
 
