959238eeb1189d3b768f190b75e0416fbe155fda
bmfranzini <101676549+bmfranzini@users.noreply.github.com>
2022-07-06 22:52:56 -0300

    Add files via upload

---
+++ Lab03_CES22/main.py
@@ -0,0 +1,8 @@
+from classes import *
+
+print("\n\nVamos testar a fabrica concreta de bolo de casamento: \n")
+client_code(FabricaBoloCasamento())
+print("\n\nVamos testar a fabrica concreta de bolo de aniversario: \n")
+client_code(FabricaBoloAniversario())
+print("\n\nVamos testar a fabrica concreta de bolo de festa informal: \n")
+client_code(FabricaBoloFestaInformal())
